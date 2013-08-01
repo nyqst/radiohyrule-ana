3f956cb82a8ebdf6c138adc9758706a73582ccc7
Lysann Schlegel <lysann.kessler@gmail.com>
2013-08-01 22:23:52 +0200

    remove some useless stuff from the Listen fragment

--- src/com/radiohyrule/android/listen/ListenFragment.java
+++ src/com/radiohyrule/android/listen/ListenFragment.java
@@ -9,9 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class ListenFragment extends BaseFragment {
-	public ListenFragment() {
-	}
-	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.fragment_listen, container, false);
