8f474681703949fab9ef539aabfe9a51e8f8e81d
Lysann Schlegel <lysann.kessler@gmail.com>
2013-08-21 17:23:27 +0200

    create layouts for smaller and larger screens in portrait orientation

--- src/com/radiohyrule/android/util/FittingSquareLayout.java
+++ src/com/radiohyrule/android/util/FittingSquareLayout.java
@@ -4,19 +4,17 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
 
-// http://stackoverflow.com/questions/2948212/android-layout-with-sqare-buttons
-public class SquareLayout extends LinearLayout {
-    public SquareLayout(Context context) {
+public class FittingSquareLayout extends LinearLayout {
+    public FittingSquareLayout(Context context) {
         super(context);
     }
 
-    public SquareLayout(Context context, AttributeSet attrs) {
+    public FittingSquareLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-
         int width = MeasureSpec.getSize(widthMeasureSpec);
         int height = MeasureSpec.getSize(heightMeasureSpec);
 
---
+++ src/com/radiohyrule/android/util/HeightFittingSquareLayout.java
@@ -0,0 +1,22 @@
+package com.radiohyrule.android.util;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class HeightFittingSquareLayout extends LinearLayout {
+    public HeightFittingSquareLayout(Context context) {
+        super(context);
+    }
+
+    public HeightFittingSquareLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int height = getMeasuredHeight();
+        setMeasuredDimension(height, height);
+    }
+}
