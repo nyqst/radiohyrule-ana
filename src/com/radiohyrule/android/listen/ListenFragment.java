44a268b96f77ebe13cb45352572716ce76359b77
Lysann Schlegel <lysann.kessler@gmail.com>
2013-08-24 00:30:06 +0200

    save and restore play/stop and favourite state

--- src/com/radiohyrule/android/listen/ListenFragment.java
+++ src/com/radiohyrule/android/listen/ListenFragment.java
@@ -1,5 +1,6 @@
 package com.radiohyrule.android.listen;
 
+import android.widget.ImageButton;
 import com.radiohyrule.android.app.BaseFragment;
 import com.radiohyrule.android.R;
 
@@ -9,9 +10,47 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class ListenFragment extends BaseFragment {
+    protected ImageButton buttonPlayStop;
+    protected static final String saveKey_buttonPlayStop_isSelected = "buttonPlayStop_isSelected";
+    protected ImageButton buttonFavouriteCurrent;
+    protected static final String saveKey_buttonFavouriteCurrent_isSelected = "buttonFavouriteCurrent_isSelected";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_listen, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_listen, container, false);
+        if(rootView == null) return null;
+
+        buttonPlayStop = (ImageButton) rootView.findViewById(R.id.listen_button_play_stop);
+        if(buttonPlayStop != null) {
+            buttonPlayStop.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    view.setSelected(!view.isSelected());
+                }
+            });
+            if(savedInstanceState != null) buttonPlayStop.setSelected(savedInstanceState.getBoolean(saveKey_buttonPlayStop_isSelected));
+        }
+
+        buttonFavouriteCurrent = (ImageButton) rootView.findViewById(R.id.listen_button_favourite_current);
+        if(buttonFavouriteCurrent != null) {
+            buttonFavouriteCurrent.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    view.setSelected(!view.isSelected());
+                }
+            });
+            if(savedInstanceState != null) buttonFavouriteCurrent.setSelected(savedInstanceState.getBoolean(saveKey_buttonFavouriteCurrent_isSelected));
+        }
+
+        return rootView;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putBoolean(saveKey_buttonPlayStop_isSelected, buttonPlayStop.isSelected());
+        outState.putBoolean(saveKey_buttonFavouriteCurrent_isSelected, buttonFavouriteCurrent.isSelected());
     }
 
     @Override
